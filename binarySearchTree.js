ce10bae0835bf829c13171ad1be785f4733c952a
hebert villafuerte <hebert.villafuerte.c@hotmail.com>
2021-01-11 09:07:27 -0500

    que es un binary search tree

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,8 +1,7 @@
 //BinarySearchTree
 
 //Los arboles son una estructura de datos que puede ser lineal o no ( ahora lo vemos),
-//los arboles tienen una estructura con nodos que estos representan
-//ser los hijos podemos ver en esta imagen todos los componentes que forman un arbol
+//los arboles tienen una estructura con nodos que estos representan ser los hijos
 
 //Por favor, no te asustes al llegar a esta clase, es muy sencillo:
 //.
@@ -17,7 +16,72 @@
 //¿Este elemento es mayor o menor que el que estoy buscando? Ohh es mayor,
 //entonces me voy para la izquierda, y así sucesivamente hasta que encuentres el elemento que estabas buscando.
 
+//cada nodo debe cumplir esta logica
+//lado izquiero disminuyendo - lado derecho aumentando
+//      10
+//   4      20
+// 2   8   17  170
+
 //metodos
 //search - buscar un nodo
 //insert - insertar un nodo
 //delete - borrar un nodo
+
+class Node {
+  constructor(value) {
+    this.left = null;
+    this.right = null;
+    this.value = value;
+  }
+}
+
+class BinarySeachTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value < currentNode.value) {
+          if (!currentNode.left) {
+            currentNode.left = newNode;
+            return this;
+          }
+          currentNode = currentNode.left;
+        } else {
+          if (!currentNode.right) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+
+  search(value) {
+    let firstNode = this.root;
+    while (true) {
+      if (firstNode.right === null && firstNode.left === null) {
+        if (value !== firstNode.value) {
+          return "No esta en el arbol";
+        }
+      }
+      if (value === firstNode.value) {
+        return firstNode;
+      } else if (value < firstNode.value) {
+        firstNode = firstNode.left;
+      } else {
+        firstNode = firstNode.right;
+      }
+    }
+  }
+}
+
+const tree = new BinarySeachTree();
